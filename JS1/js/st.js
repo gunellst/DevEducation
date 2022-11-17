572e9fdd535ffc9d58feb350dc99bab372a3adc2
Gunel <116121857+gunellst@users.noreply.github.com>
2022-11-17 09:47:29 +0400

    Update st.js

--- JS1/js/st.js
+++ JS1/js/st.js
@@ -1,17 +1,26 @@
+// 1. User gives 2 numbers (A and B). Print to the console the result of dividing A by B and the remainder of the division.
+// 2. The user enters 2 values (A and B). Swap the contents of variables A and B (that means the value of variable B must be assigned to variable A, and vice versa).
+// 3. The user enters 3 non-zero numbers (A, B and C). Print to the console the solution (the value of X) of the standard linear equation, where A*X+B=C.
+// 4. User gives 2 numbers (A and B). If A>B then print to the console the result of A+B, if A=B then the result of A*B, if A<B then A-B.
+// 5. User gives 3 numbers (a, b, c). Find out and print to the console the solution of quadratic equation ax^2+bx+c=0.
+// 6. The user enters a two-digit number. Print the literal representation of this number to the console. For example, typing "25" will display "twenty five" in the console, and "13" will display "thirteen".
+// 7. User gives 3 numbers (x, y, r). Find out if point with location (x,y) is inside a circle with radius r? (the center of the circle is at the origin)
+
+
 // 1. User gives 2 numbers (A and B). Print to the console the result of dividing A by B and the remainder of the division. 
  
-let A=+prompt("enter A number:"); 
-let B=+prompt("enter B number:"); 
+// let A=+prompt("enter A number:"); 
+// let B=+prompt("enter B number:"); 
  
-if(B==0) { 
-    alert('Dividing by zero is undefined') 
-} 
-else { 
-    let result=A/B; 
-    let remainder=A%B; 
-    console.log('result='+" "+ result); 
-    console.log('remainder='+" "+ remainder); 
-}
+// if(B==0) { 
+//     alert('Dividing by zero is undefined') 
+// } 
+// else { 
+//     let result=A/B; 
+//     let remainder=A%B; 
+//     console.log('result='+" "+ result); 
+//     console.log('remainder='+" "+ remainder); 
+// }
 
 
 // (1. with function)
