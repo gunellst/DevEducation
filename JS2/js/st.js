d274f031236dbd51ebb292eab9b279de3357afd1
Gunel <116121857+gunellst@users.noreply.github.com>
2022-11-17 09:48:36 +0400

    Update st.js

--- JS2/js/st.js
+++ JS2/js/st.js
@@ -1,15 +1,25 @@
 // 1. The user enters 1 number (A). Print all numbers from 1 to 1000 that are divisible by A.
-let A=+prompt("Enter the number:");
-if(A===0) {
-    alert('Dividing by zero is undefined');
-}
-else{
-    for(let i=1; i<=1000; i++) {
-        if (i%A==0){
-            console.log(i);
-        }
-    }
-}
+// 2. The user enters 1 number (A). Find the number of positive integers whose square is less than A (n^2 < A).
+// 3. The user enters 1 number (A). Print the largest divisor (other than A itself) of the number A.
+// 4. The user enters 2 numbers (A and B). Print the sum of all numbers from the range A to B that are divisible by 7 without a remainder. (Note that B may be less than A if you type).
+// 5. The user enters 1 positive number (N). Print the Nth number of the fibonacci series. In the fibonacci series, each next number is the sum of the previous two. The first and second are considered equal to 1.
+// 6. The user enters 2 numbers. Find their greatest common divisor using Euclid's algorithm. (yes, you've got to google this algorithm at first :D ).
+// 7. The user enters 1 number. Find a number that is a mirror image of the sequence of digits of a given number, for example, given the number 123, output is 321 in this case.
+// 8. The user enters 2 numbers. Find out if these two numbers have the same digits. For example, for a pair of 123 and 3456789, the answer would be "YES", and for a pair of 500 and 99, "NO".
+
+
+// // 1. The user enters 1 number (A). Print all numbers from 1 to 1000 that are divisible by A.
+// let A=+prompt("Enter the number:");
+// if(A===0) {
+//     alert('Dividing by zero is undefined');
+// }
+// else{
+//     for(let i=1; i<=1000; i++) {
+//         if (i%A==0){
+//             console.log(i);
+//         }
+//     }
+// }
 
 
 // (1. with function)
